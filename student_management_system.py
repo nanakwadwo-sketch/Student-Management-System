dbdb40df4d3b1ffcb74dee2ccc37d888cd4b0857
nankwadwo-sketch <simtechgh@gmail.com>
2025-01-21 14:24:53 -0800

    added the display function

---
+++ student_management_system.py
@@ -0,0 +1,13 @@
+students ={}
+
+def display_menu():
+    
+    print("\nStudent Management System")
+    print("1. Add Student")
+    print("2. View All Students")
+    print("3. Update Student")
+    print("4. Delete Student")
+    print("5. Search Student")
+    print("6. View Unique Courses")
+    print("7. Exit")
+   
